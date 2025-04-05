3a6b1ce79a158fde278a5d021f90bdf88a82a642
javohir <yjavohir543@gmail.com>
2025-04-05 15:37:43 +0500

    vazifa

---
+++ js/data.js
@@ -0,0 +1,18 @@
+export default [
+  
+  {
+    title: "coffe strit",
+    github: "https://github.com/Javohirbmw/3dars",
+    vercel: "https://3dars-khaki.vercel.app/",
+  },
+  {
+    title: "Ansormed",
+    github: "https://github.com/Javohirbmw/10.vazifa",
+    vercel: "https://10-vazifa-orcin.vercel.app/",
+  },
+  {
+    title: "Garage",
+    github: "https://github.com/Javohirbmw/garage",
+    vercel: "https://garage-peach-two.vercel.app/",
+  },
+];
